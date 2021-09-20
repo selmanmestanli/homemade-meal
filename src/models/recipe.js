ba2a585efc1cf4968cf5de1c53676ed8f679e31a
selmanmestanli <selmanmestanli@gmail.com>
2021-09-20 12:40:05 +0200

    Convert recipe class into mongoose schema enabling autopopulate

--- src/models/recipe.js
+++ src/models/recipe.js
@@ -1,9 +1,17 @@
-class Recipe {
-  constructor(recipename) {
-    this.recipename = recipename
-    this.likedBy = []
-    this.addedBy = ''
-  }
-}
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 
-module.exports = Recipe
+const recipeSchema = new mongoose.Schema({
+  recipename: String,
+  likedBy: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      autopopulate: true,
+    },
+  ],
+  addedBy: String,
+})
+
+recipeSchema.plugin(autopopulate)
+module.exports = mongoose.model('Recipe', recipeSchema)
