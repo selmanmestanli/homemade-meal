b4e53437c6fdfd2c8698e5b8e614aa5c12d84a8b
selmanmestanli <selmanmestanli@gmail.com>
2021-09-29 02:07:25 +0200

    Update end point path adding /api

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,11 +33,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/photos', photosRouter)
-app.use('/recipes', recipesRouter)
-app.use('/comments', commentRouter)
+app.use('/api/', indexRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/photos', photosRouter)
+app.use('/api/recipes', recipesRouter)
+app.use('/api/comments', commentRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
