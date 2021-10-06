9b7bf2feb45f2371823d118c327ee86fc20e9597
selmanmestanli <selmanmestanli@gmail.com>
2021-10-06 16:07:56 +0200

    Create user-detail vue file and apply mapActions from vuex library

---
+++ frontend/src/views/user-detail.vue
@@ -0,0 +1,28 @@
+<script>
+import UserCard from '@/components/user-card.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'UserDetail',
+  components: { UserCard },
+  data() {
+    return {
+      user: null,
+    }
+  },
+  async created() {
+    // const userRequest = await axios.get('/api/users/${this.$route.params.id}')
+    // this.user = userRequest.data
+    this.user = await this.fetchUser(this.$route.params.id)
+  },
+  methods: {
+    ...mapActions(['fetchUser']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .about
+    h1 This is a user-detail
+    UserCard(:user="user" v-if="user")
+</template>
