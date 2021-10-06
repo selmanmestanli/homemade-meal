49dd4c1f9ca53b4102cc1b1e97574a9417a9cbcb
selmanmestanli <selmanmestanli@gmail.com>
2021-10-06 16:08:49 +0200

    Create userList vue file and apply mapActions from vuex library

---
+++ frontend/src/views/user-list.vue
@@ -0,0 +1,27 @@
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'UserList',
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    // const usersRequest = await axios.get('/api/users')
+    this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Homemade-Meal
+    h2 Users
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
+</template>
