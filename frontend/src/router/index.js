95c19f67f3482563e0a62238f175fd1244bf3c66
selmanmestanli <selmanmestanli@gmail.com>
2021-10-06 16:02:50 +0200

    Add userDetail router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,12 +11,12 @@ const routes = [
     component: UserList,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/users/:id',
+    name: 'UserDetail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/user-detail.vue'),
   },
 ]
 
