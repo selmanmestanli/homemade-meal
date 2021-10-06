5db14d97df0017a1841203bfbaca145c2f6a4979
selmanmestanli <selmanmestanli@gmail.com>
2021-10-06 15:55:31 +0200

    Add user list as router link and delete the default links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
   #app
     #nav
-      router-link(to="/") Home |&nbsp;
-      router-link(to="/about") About
+      router-link(to="/") User list
     router-view
 </template>
 
