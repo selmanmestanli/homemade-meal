bb338d602f8c4b845c8063a5286dc72074005448
selmanmestanli <selmanmestanli@gmail.com>
2021-10-06 16:43:15 +0200

    Add recipes section to the user-card component

--- frontend/src/components/user-card.vue
+++ frontend/src/components/user-card.vue
@@ -2,13 +2,18 @@
 export default {
   name: 'UserCard',
   props: ['user'],
+  /* data() {
+    return {
+      defaultBio: 'This user has no bios.'
+    }
+  }*/
 }
 </script>
 
 <template lang="pug">
 .box
   h1 {{ user.name }}
-  h3 {{ user.email }}
+  p {{ user.email }}
   h2 Photos ({{ user.photos.length }})
 
   .photo(v-for="photo in user.photos")
@@ -17,6 +22,14 @@ export default {
       | no likes yet!
     p(v-else)
       | Liked by: {{ photo.likedBy.map(user => user.name).join(', ') }}
+
+  h2 Recipes ({{ user.recipes.length }})
+  .recipe(v-for="recipe in user.recipes")
+    h3 {{ recipe.recipename }}
+    p(v-if="!recipe.likedBy.length")
+      | no likes yet!
+    p(v-else)
+      | Liked by: {{ recipe.likedBy.map(user => user.name).join(', ') }}
 </template>
 
 <style lang="scss" scoped>
