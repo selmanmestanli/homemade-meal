0421aa9256f1e5c57e873b9f1de5d4706f3f4b90
selmanmestanli <selmanmestanli@gmail.com>
2021-10-07 17:55:40 +0200

    Implement express-session and connect-mongo

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,10 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-require('./database-connection')
+const mongooseConnection = require('./database-connection')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -31,6 +33,16 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(
+  session({
+    secret: ['thisisnotasupersecuresecretsecret', 'thisisanothersupernotsosecretsecret'],
+    store: new MongoStore({ mongooseConnection, stringify: false }),
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api/', indexRouter)
