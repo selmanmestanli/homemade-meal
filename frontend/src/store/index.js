a464b3652cdebf7b436e6a642875b605816ed4f8
selmanmestanli <selmanmestanli@gmail.com>
2021-10-27 12:14:06 +0200

    Implement socket.io into frontend by defining relevant mutations and actions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,21 +2,42 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+import io from 'socket.io-client'
+
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 axios.defaults.withCredentials = true
 
 
 Vue.use(Vuex)
 
+const socket = io(process.env.VUE_APP_BASE_URL)
+
 const mutations = {
   SET_USER: 'set user',
+  SET_LIVE_STREAM: 'set live stream',
+  ADD_LIVE_STREAM: 'add live stream',
+  ADD_MESSAGE_TO_LIVE_STREAM: 'add message to live stream',
 }
 
 const store = new Vuex.Store({
-  state: {},
+  state: {
+    user: null,
+    currentLiveStream: null,
+    liveStreams: [],
+    liveStreamMessages: [],
+  },
   mutations: {
     [mutations.SET_USER](state, user) {
       state.user = user
+    },
+    [mutations.SET_LIVE_STREAM](state, live) {
+      state.currentLiveStream = live
+    },
+    [mutations.ADD_LIVE_STREAM](state, stream) {
+      state.liveStreams.push(stream)
+    },
+    [mutations.ADD_MESSAGE_TO_LIVE_STREAM](state, message) {
+      state.liveStreamMessages.push(message)
     },
   },
   actions: {
@@ -47,8 +68,36 @@ const store = new Vuex.Store({
       await axios.delete('/api/account/session')
       commit(mutations.SET_USER, null)
     },
+    async goLive({ state, commit }) {
+      socket.emit('go live', state.user._id, status => {
+        commit(mutations.SET_LIVE_STREAM, state.user._id)
+      })
+    },
+    async addLiveStream({ state, commit }, stream) {
+      commit(mutations.ADD_LIVE_STREAM, stream)
+    },
+    async sendMessageToLiveStream({ state, commit }, body) {
+      const message = {
+        body,
+        author: state.user.name,
+      }
+      commit(mutations.ADD_MESSAGE_TO_LIVE_STREAM, message)
+      socket.emit('new message', state.currentLiveStream, message)
+    },
+    async joinStream({ state, commit }, stream) {
+      socket.emit('join stream', stream)
+      commit(mutations.SET_LIVE_STREAM, stream)
+    },
   },
   modules: {},
+})
+
+socket.on('new live stream', user => {
+  store.dispatch('addLiveStream', user)
+})
+
+socket.on('new live stream message', message => {
+  store.commit(mutations.ADD_MESSAGE_TO_LIVE_STREAM, message)
 })
 
 export default async function init() {
